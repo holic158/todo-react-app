2c272771b3f177d0e8d58cf3a8d909e6df7a999d
youngmin <holic158@naver.com>
2023-01-30 23:34:53 +0900

    fst commit

---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+const Todo = () => {
+    return (
+        <div ClassName="Todo">
+            <input tpye="checkbox" id="todo0" name="todo0" value="todo0" />
+            <label for="todo0">Todo 컴포넌트 만들기</label>
+        </div>
+    )
+}
+
+export default Todo;
