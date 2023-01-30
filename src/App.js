fa93e1fb0e543fd3d33a24a7e7aa7e529744d5a7
youngmin <holic158@naver.com>
2023-01-31 01:22:10 +0900

    AddTodo 화면

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Todo from './Todo';
 import React, { useState } from "react"
+import { Container, List, Paper } from "@mui/material" 
+import AddTodo from './AddTodo';
 
 function App() {
   const [items, setItem] = useState([
@@ -16,14 +18,21 @@ function App() {
     }
   ]);
   
-  let todoItems =
-    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
-
-  return (
-    <div className="App">
-      {todoItems}
-    </div>
-  );
+  let todoItems = items.length > 0 && (
+    <Paper style={{margin: 16}}>
+      <List>
+        {items.map((item) => (
+          <Todo item={item} key={item.id} />
+        ))};
+      </List>
+    </Paper>
+  )                
+  return <div className="App">
+          <Container maxWidth="md">
+            <AddTodo />
+            <div className='TodoList'>{todoItems}</div>
+          </Container>
+          </div>;
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,19 +1,25 @@
 
 import React, { useState} from "react";
+import { ListItem, ListItemText, inputBase, Checkbox, InputBase } from "@mui/material"
 
 const Todo = (props) => {
     const [item, setItem] = useState(props.item);
 
     return (
-        <div ClassName="Todo">
-            <input
-                type="checkbox"
-                id={item.id}
-                name={item.id}
-                value={item.done}
-            />
-            <label id={item.id}>{item.title}</label>
-        </div>
+        <ListItem>
+            <Checkbox checked={item.done} />
+            <ListItemText>
+                <InputBase
+                    inputProps={{ "aria-label": "naked"}}
+                    type="text"
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true}
+                />
+            </ListItemText>
+        </ListItem>
     )
 }
 
